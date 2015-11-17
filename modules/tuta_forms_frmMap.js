a5874e3d796235993785d7f5506b652c094f1aa2
Evert-jvv <trevejvv@gmail.com>
2015-11-17 16:59:07 +0200

    Merged code from previous push
    Merged code from previous push that got lost.

--- modules/tuta_forms_frmMap.js
+++ modules/tuta_forms_frmMap.js
@@ -16,6 +16,26 @@ tuta.forms.frmMap = function() {
   
   tuta.forms.frmMap.onPreShow = function(form) {
     var self = this;
+    
+    
+    
+      this.leftMenu = new tuta.controls.menu(
+         this.control("flexAll"), 
+         this.control("flexMenu"), 
+         tuta.controls.position.LEFT,
+         tuta.controls.behavior.MOVE_OVER,
+         0.25
+     );	
+   
+   // this.control("btnChs").onClick = function (button) {tuta.forms.frmMap.show();};
+   this.control("btnChs").onClick = function(button) {
+       self.leftMenu.toggle();
+     };
+    this.control("btnPay").onClick = function (button) {tuta.forms.frmPayments.show();};
+    this.control("btnHistory").onClick = function (button) {tuta.forms.frmTrip.show();};
+    this.control("btnHelp").onClick = function (button) {tuta.forms.frmHelp.show();};
+    this.control("btnPromo").onClick = function (button) {tuta.forms.frmPromo.show();};
+    this.control("btnAbout").onClick = function (button) {tuta.forms.frmAbout.show();};
    // PUT BUTTONS HERE
   };
   
