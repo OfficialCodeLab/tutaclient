03c80f152a76d76fad289689daec977c2d648faa
Courtney Jooste <courtney@codelab.io>
2015-12-06 12:10:11 +0200

    Reversal of temporary changes - removed actions and annotations

--- actions/mobile/AS_Button_3d4444ceeb94421bb8851ccf00a97921.js
+++
@@ -1,4 +0,0 @@
-function AS_Button_3d4444ceeb94421bb8851ccf00a97921(eventobject) {
-frmCodeLab.show();
-
-}
--- actions/mobile/AS_Button_fc13a652ea724400a89306899ce05a71.js
+++
@@ -1,4 +0,0 @@
-function AS_Button_fc13a652ea724400a89306899ce05a71(eventobject) {
-frmCodeLab.show();
-
-}
